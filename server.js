2bf5902a4f22a1f1f975cec9bdb70ce3f74645d0
Gagan <100528782+Gagandeep010@users.noreply.github.com>
2023-06-26 19:05:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
 const app = express();
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json())
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'index.html'));
 });
@@ -30,21 +23,3 @@ app.post('/submit', (req, res) => {
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
-
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-
-// app.use(express.static(path.join(__dirname,'index.html')));
-
-// app.post('/submit', (req, res) => {
-//   const { name, age } = req.body;
-//   res.send("Submitted Form Data: Name - ${name}, Age - ${age}");
-// });
-
-// app.listen(3000, () => {
-//   console.log('Server is running on port 3000');
-// });
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname,'index.html'));
-// });
